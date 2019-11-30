0a5f079a3ddfd21d725034b807332c0af4f64617
Randy Pen <orz@f2cafe.com>
2019-11-30 14:19:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,104 @@
+# 短文本聚类
+
+### 项目介绍
+短文本聚类是常用的文本预处理步骤，可以用于洞察文本常见模式、分析设计语义解析规范等。本项目实现了内存友好的短文本聚类方法。
+
+
+
+### 依赖库
+
+>  pip install tqdm jieba
+
+
+
+### 使用方法
+
+```bash
+python cluster.py --infile ./data/infile \
+--output ./data/output
+```
+
+具体参数设置可以参考```cluster.py```文件内```_get_parser()```函数参数说明，包含设置分词词典、停用词、匹配采样数、匹配度阈值等。
+
+
+
+### 文件路径
+
+```html
+TextCluster
+|      README.md
+|      LICENSE
+|      cluster.py                    主要执行程序
+|      
+|------utils                         公共功能模块
+|    |    __init__.py
+|    |    segmentor.py               分词器封装
+|    |    similar.py                 相似度计算函数
+|    |    utils.py                   文件处理模块
+|
+|------data
+|    |    infile                     默认输入文本路径，用于测试中文模式
+|    |    infile_en                  默认输入文本路径，用于测试英文模式
+|    |    seg_dict                   默认分词词典
+|    |    stop_words                 默认停用词路径
+```
+
+
+
+注：本方法仅面向短文本，长文本聚类可根据需求选用[SimHash](https://en.wikipedia.org/wiki/SimHash), [LDA](https://en.wikipedia.org/wiki/Latent_Dirichlet_allocation)等其他算法。
+
+
+
+# Text Cluster
+
+### Introduction
+
+Text cluster is a normal preprocess method to analysis text feature. This project implements a memory friendly method for **short text cluster**. For long text, it will be preferable to choose [SimHash](https://en.wikipedia.org/wiki/SimHash) or [LDA](https://en.wikipedia.org/wiki/Latent_Dirichlet_allocation) or others according to demand.
+
+
+
+### Requirements
+
+>  pip install tqdm spacy
+
+
+
+### Usage
+
+```bash
+python cluster.py --infile ./data/infile_en \
+--output ./data/output \
+--lang en
+```
+
+
+
+### File Structure
+
+```html
+TextCluster
+|      README.md
+|      LICENSE
+|      cluster.py                    main excutable function
+|      
+|------utils                         utilities
+|    |    __init__.py
+|    |    segmentor.py               tokenizer wrapper
+|    |    similar.py                 similarity calculator
+|    |    utils.py                   file process module
+|
+|------data
+|    |    infile                     default input file path, to test Chinese mode
+|    |    infile_en                  default input file path, to test English mode
+|    |    seg_dict                   default tokenizer dict path
+|    |    stop_words                 default stop words path
+```
+
+
+
+
+
+# Other Language
+
+For other specific language, modify tokenizer wrapper in ```./utils/segmentor.py```. 
+
---
+++ cluster.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import os
+import argparse
+from collections import defaultdict
+
+from tqdm import tqdm
+
+from utils.similar import jaccard
+from utils.segmentor import Segmentor
+from utils.utils import check_file, ensure_dir, clean_dir, sample_file, get_stop_words, line_counter
+
+
+def _get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--infile', type=str, default='./data/infile', help='Directory of input file.')
+    parser.add_argument('--output', type=str, default='./data/output', help='Directory to save output file.')
+    parser.add_argument('--dict', type=str, default='./data/seg_dict', help='Directory of dict file.')
+    parser.add_argument('--stop_words', type=str, default='./data/stop_words', help='Directory of stop words.')
+    parser.add_argument('--sample_number', type=int, default=5, help='Sample number for each bucket.')
+    parser.add_argument('--threshold', type=float, default=0.3, help='Threshold for matching.')
+    parser.add_argument('--name_len', type=int, default=9, help='Filename length.')
+    parser.add_argument('--name_len_update', type=bool, default=False, help='To update file name length.')
+    parser.add_argument('--lang', type=str, choices=['cn', 'en'], default='cn', help='Segmentor language setting.')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = _get_parser()
+
+    # preliminary work
+    check_file(args.infile)
+    ensure_dir(args.output)
+
+    if args.name_len_update:
+        line_cnt = line_counter(args.infile)
+        args.name_len = len(str(line_cnt)) + 1
+
+    clean_dir(args.output, args.name_len)
+    # end preliminary work
+
+    p_bucket = defaultdict(list)
+    save_idx = 0
+    id_name = '{0:0' + str(args.name_len) + 'd}'
+    # load stop words
+    stop_words = get_stop_words(args.stop_words) if os.path.exists(args.stop_words) else list()
+    # load tokenizer
+    seg = Segmentor(args)
+
+    print('Splitting sentence into different clusters ...')
+    infile = open(args.infile, 'r', encoding="utf-8")
+    for line in tqdm(infile):
+        line = line.rstrip()
+        is_match = False
+        seg_list = list(seg.cut(line))
+        if stop_words:
+            seg_list = list(filter(lambda x: x not in stop_words, seg_list))
+        for wd in seg_list:
+            w_bucket = p_bucket[wd]
+            # is_match = False
+            for bucket in w_bucket:
+                bucket_path = os.path.join(args.output, bucket)
+                check_file(bucket_path)
+                selected = sample_file(bucket_path, args.sample_number)
+                selected = list(map(lambda x: list(seg.cut(x)), selected))
+                if stop_words:
+                    filt_selected = list()
+                    for sen in selected:
+                        sen = list(filter(lambda x: x not in stop_words, sen))
+                        filt_selected.append(sen)
+                    selected = filt_selected
+                if all(jaccard(seg_list, cmp_list) > args.threshold for cmp_list in selected):
+                    is_match = True
+                    with open(bucket_path, 'a', encoding='utf-8') as outfile:
+                        outfile.write(line+'\n')
+                    break
+            if not is_match:
+                bucket_name = ('tmp' + id_name).format(save_idx)
+                w_bucket.append(bucket_name)
+                bucket_path = os.path.join(args.output, bucket_name)
+                with open(bucket_path, 'a', encoding='utf-8') as outfile:
+                    outfile.write(line+'\n')
+                save_idx += 1
+                break
+
+    infile.close()
+
+    # sort and rename file
+    file_list = os.listdir(args.output)
+    file_list = list(filter(lambda x: x.startswith('tmp'), file_list))
+    cnt = dict()
+    for file in file_list:
+        file_path = os.path.join(args.output, file)
+        cnt[file] = line_counter(file_path)
+
+    sorted_cnt = sorted(cnt.items(), key=lambda kv: kv[1], reverse=True)
+    for idx, (file_name, times) in enumerate(sorted_cnt):
+        origin_path = os.path.join(args.output, file_name)
+        new_path = os.path.join(args.output, id_name.format(idx))
+        os.rename(origin_path, new_path)
+
+    print('All is well')
+
+
+if __name__ == '__main__':
+    main()
